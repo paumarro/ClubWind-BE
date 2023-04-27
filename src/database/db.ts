a40cc9b5ce9ae353fc9b7dd98015b6f3c7dbf454
Pau Marro <paumarro98@gmail.com>
2023-04-27 17:52:39 +0200

    https before host name db

--- src/database/db.ts
+++ src/database/db.ts
@@ -2,20 +2,14 @@ import { Sequelize } from 'sequelize'
 import fs from 'fs';
 import path from 'path';
 
-const dbHost = "clubwinddb.mysql.database.azure.com"
+const dbHost = "https://clubwinddb.mysql.database.azure.com"
 const dbName = "clubWinddb"
 const dbUser = "cwADMIN"
 const dbPass = "ClubWind123"
 
-console.log('DB_HOST:', process.env.DB_HOST);
-console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
-console.log('DB_NAME:', process.env.DB_NAME);
-
-// Load the CA certificate file provided by Azure
+// CA certificate file provided by Azure
 const caCert = fs.readFileSync(path.resolve(__dirname,'./DigiCertGlobalRootCA.crt.pem'));
 
-// Instantiate a new Sequelize instance for the main data
 export const sequalize: Sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
   dialect: 'mysql',
