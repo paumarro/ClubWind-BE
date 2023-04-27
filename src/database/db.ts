d741d6ef9a8ee513a79b56bb881d866cb23df5ad
Pau Marro <paumarro98@gmail.com>
2023-04-27 16:54:22 +0200

    remove .env

--- src/database/db.ts
+++ src/database/db.ts
@@ -7,6 +7,12 @@ const dbName = process.env.DB_NAME || "clubWinddb"
 const dbUser = process.env.DB_USER || "cwADMIN"
 const dbPass = process.env.DB_PASSWORD
 
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+
 // Load the CA certificate file provided by Azure
 const caCert = fs.readFileSync(path.resolve(__dirname,'./DigiCertGlobalRootCA.crt.pem'));
 
